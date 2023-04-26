c10d29bb1bf5ce718342ec009ab899f182d53935
Fathur Rahman K <fathur2110@gmail.com>
2023-04-26 17:14:00 +0700

    menambahkan API Movies

---
+++ modul9_1302210063/Controllers/Movie.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Reflection;
+using modul9_1302210063;
+using modul9_1302210063.Properties;
+using System.IO;
+using System.Runtime.InteropServices;
+using modul9_1302210063.Properties;
+
+
+namespace modul9_1302210063.Controllers.Movie
+{
+    [ApiController]
+    [Route("api/Movies")]
+    public class Movie : ControllerBase
+    {
+        public static List<MovieProperties> data = new List<MovieProperties>()
+        {
+            new MovieProperties("jarwo sopo", "supri", new List<string>(){"udin,baban"}, "bala bala"),
+
+
+        };
+        [HttpGet]
+        public IEnumerable<MovieProperties> Get()
+        {
+            return data;
+        }
+        [HttpGet("{id}")]
+        public MovieProperties Get(int id)
+        {
+            return data[id];
+        }
+        [HttpPost]
+        public void Post([FromBody] MovieProperties value)
+        {
+            data.Add(value);
+
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int jdl)
+        {
+            if (jdl < 0 || jdl >= data.Count)
+            {
+                NotFound();
+            }
+            data.RemoveAt(jdl);
+        }
+        private void NotFound()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ modul9_1302210063/Properties/MovieProperties.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace modul9_1302210063.Properties
+{
+    public class MovieProperties
+    {
+        public String title { get; set; }
+        public String director { get; set; }    
+        public List<String> stars { get; set; }
+        public String description { get; set; }
+
+        public MovieProperties(string title, string director, List<string> stars, string description) 
+        {
+            this.stars = stars;
+            this.title = title;
+            this.director = director;
+            this.description = description;
+        }
+    }
+}
